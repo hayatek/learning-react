9828d3018d72f9e73d9e5e48d1f23d316cd4ecbd
hayatek <stream.hyt@gmail.com>
2022-02-27 08:59:05 +0900

    added var-and-let.js

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # learning-react
 React学習用のリポジトリ
+
+#ファイル
+- var-and-let.js
+varとletの違いを確認
---
+++ var-and-let.js
@@ -0,0 +1,16 @@
+var strVar = "var変更前文字列";
+console.log(strVar); //var変更前文字列
+strVar = "var変更後文字列";
+console.log(strVar); //var変更後文字列
+
+let strLet = "let変更前文字列";
+console.log(strLet); //let変更前文字列
+strLet = "let変更後文字列";
+console.log(strLet); //let変更後文字列
+
+//varは再宣言可能
+var strVar = "var再宣言文字列";
+console.log(strVar); //var再宣言文字列
+
+//letは再宣言可能
+//let strLet = "let再宣言文字列";
