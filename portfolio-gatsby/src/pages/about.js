9c35ba843308969057244d018df4e63c353a1860
hayatek <stream.hyt@gmail.com>
2022-04-10 22:19:29 +0900

    update pages

---
+++ portfolio-gatsby/src/pages/about.js
@@ -0,0 +1,27 @@
+import React from "react"
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus libero
+        perspiciatis est architecto quibusdam, iusto aliquam! Vero dolores
+        doloremque labore eum inventore ex dolore qui. Error cupiditate
+        repudiandae esse voluptatum.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus libero
+        perspiciatis est architecto quibusdam, iusto aliquam! Vero dolores
+        doloremque labore eum inventore ex dolore qui. Error cupiditate
+        repudiandae esse voluptatum.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus libero
+        perspiciatis est architecto quibusdam, iusto aliquam! Vero dolores
+        doloremque labore eum inventore ex dolore qui. Error cupiditate
+        repudiandae esse voluptatum.
+      </p>
+    </div>
+  )
+}
---
+++ portfolio-gatsby/src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Home() {
+  return (
+    <section>
+      <div>
+        <h2>Design</h2>
+        <h3>Develop & Deploy</h3>
+        <p>I'm a software engineer</p>
+      </div>
+    </section>
+  )
+}
